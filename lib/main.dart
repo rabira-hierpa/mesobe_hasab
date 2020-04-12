46678241207a0e35b3c12938d13512377da44fec
Rabra <rabira.hierpa@gmail.com>
2020-04-12 22:24:33 +0300

    Added a basic home screen template

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mesobe_hasab/pages/HomeScreen.dart';
 import 'package:mesobe_hasab/pages/splash.dart';
 
 void main() => runApp(MaterialApp(
-    initialRoute: '/',
+    initialRoute: '/home',
     routes:{
       '/': (context) => SplashScreen(),
+      '/home': (context) => HomeScreen()
     }
 ));
-
 
