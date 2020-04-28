f92e81822681cf9a4c81680ba2244b28832dc6ef
Rabra <rabira.hierpa@gmail.com>
2020-04-28 21:01:46 +0300

    Added a share button on the ViewQutoe Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mesobe_hasab/pages/ViewQuote.dart';
 import 'package:mesobe_hasab/pages/info.dart';
-import 'package:mesobe_hasab/pages/splash.dart';
+//import 'package:mesobe_hasab/pages/splash.dart';
 import 'pages/home.dart';
 
 void main() => runApp(MaterialApp(
