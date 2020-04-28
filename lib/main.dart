b9404e7e1bb2db931826aafdb3dae4bf6e217aa6
Rabra <rabira.hierpa@gmail.com>
2020-04-28 18:47:40 +0300

    Added the View Qutoes Route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mesobe_hasab/pages/ViewQuote.dart';
 import 'package:mesobe_hasab/pages/info.dart';
 import 'package:mesobe_hasab/pages/splash.dart';
 import 'pages/home.dart';
@@ -6,5 +7,5 @@ import 'pages/home.dart';
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      routes: {'/': (context) => QuoteList(), '/info': (context) => InfoPage()},
+      routes: {'/': (context) => QuoteList(), '/info': (context) => InfoPage(), '/viewQuote': (context) => ViewQuote()},
     ));
