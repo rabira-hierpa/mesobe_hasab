7b6420904aa6dc0d51627647ce457dc0716e76db
Kiya <tokiyatolcha@gmail.com>
2020-05-01 12:02:30 +0300

    ViewQoute is converted to Stateful widget.

--- lib/pages/ViewQuote.dart
+++ lib/pages/ViewQuote.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ViewQuote extends StatelessWidget {
+class ViewQuote extends StatefulWidget {
+  @override
+  _ViewQuoteState createState() => _ViewQuoteState();
+}
+
+class _ViewQuoteState extends State<ViewQuote> {
+  double _scale=1.0;
+  double _previousScale=1.0;
   Map imageData = {};
   @override
   Widget build(BuildContext context) {
@@ -14,4 +21,5 @@ class ViewQuote extends StatelessWidget {
       ),
     );
   }
+
 }
