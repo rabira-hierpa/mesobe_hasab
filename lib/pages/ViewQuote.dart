db611b5a1ef643bd06be41ecb8cb32050576fe73
Kiya <tokiyatolcha@gmail.com>
2020-05-01 12:55:22 +0300

    In ViewQoute A gesture detector and transform widget added for the future of zoom to the picture.

--- lib/pages/ViewQuote.dart
+++ lib/pages/ViewQuote.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 class ViewQuote extends StatefulWidget {
   @override
@@ -13,10 +14,38 @@ class _ViewQuoteState extends State<ViewQuote> {
   Widget build(BuildContext context) {
     imageData = ModalRoute.of(context).settings.arguments;
     return Container(
-      child: Hero(
-        tag: 'img'+imageData['img'].toString(),
-        child: Image.asset(
-            imageData['img']
+      child: GestureDetector(
+        onScaleStart: (ScaleStartDetails details){
+//          if(_scale<1.0)
+//          _previousScale=1.0;
+//          else
+            _previousScale=_scale;
+          print(_previousScale);
+          setState(() {});
+        },
+        onScaleUpdate: (ScaleUpdateDetails details){
+
+          _scale=_previousScale * details.scale;
+          print("form update $_scale");
+          setState(() {});
+        },
+        onScaleEnd: (ScaleEndDetails details){
+          if(_previousScale < 1.0)
+          _scale=1.0;
+          else
+            _previousScale=1.0;
+
+          setState(() {});
+        },
+    child: Transform(
+    alignment: FractionalOffset.center,
+    transform: Matrix4.diagonal3(Vector3(_scale,_scale,_scale)),child: Hero(
+          tag: 'img'+imageData['img'].toString(),
+
+            child: Image.asset(
+                imageData['img']
+            ),
+          ),
         ),
       ),
     );
