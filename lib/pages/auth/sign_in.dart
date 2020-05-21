7c3ed696d1dc0792d26338ef3527ad3ff7f3c633
Rabra <rabira.hierpa@gmail.com>
2020-05-21 20:11:56 +0300

    Added basic form
    Added basic email and password form

--- lib/pages/auth/sign_in.dart
+++ lib/pages/auth/sign_in.dart
@@ -9,6 +9,10 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _authService = AuthService();
+  //var email and pwd
+  String email = '';
+  String pwd = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,55 +23,95 @@ class _SignInState extends State<SignIn> {
         elevation: 0.0,
         backgroundColor: Color.fromRGBO(18, 17, 111, 10),
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton.icon(
-                  color: Color.fromRGBO(246, 155, 15, 1),
-                  onPressed: () {},
-                  icon: Icon(Icons.vpn_key),
-                  label: Text(
-                    'Sigin In',
-                    style: TextStyle(color: Colors.white, fontSize: 26),
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton.icon(
-                  color: Color.fromRGBO(246, 155, 15, 1),
-                  onPressed: () {},
-                  icon: Icon(Icons.vpn_key),
-                  label: Text(
-                    'Google Sign In',
+      body: ListView(children: <Widget>[
+        Container(
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                // Email and password sign in Form
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 20.0, horizontal: 50),
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: const InputDecoration(
+
+                          icon: Icon(Icons.alternate_email),
+                          hintText: 'someone@mail-provider.com',
+                          labelText: 'Email *',
+                          counterStyle: TextStyle(
+                            color: Colors.white
+                          )
+                        ),
+                        onChanged: (value) => {setState(() => email = value)},
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        onChanged: (value) {
+                          setState(() => pwd = value);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                // Email and password sign in button
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton.icon(
+                      color: Color.fromRGBO(246, 155, 15, 1),
+                      onPressed: () {},
+                      icon: Icon(Icons.vpn_key),
+                      label: Text(
+                        'Sigin In',
+                        style: TextStyle(color: Colors.white, fontSize: 26),
+                      )),
+                ),
+                // Google sign in button
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton.icon(
+                      color: Color.fromRGBO(246, 155, 15, 1),
+                      onPressed: () {},
+                      icon: Icon(Icons.vpn_key),
+                      label: Text(
+                        'Google Sign In',
+                        maxLines: 3,
+                        softWrap: true,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26,
+                        ),
+                      )),
+                ),
+                // Anon sign in button
+                FlatButton(
+                  onPressed: () async {
+                    Navigator.pushReplacementNamed(context, '/loading');
+                  },
+                  child: Text(
+                    'Skip for now',
                     maxLines: 3,
                     softWrap: true,
                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 26,
-                    ),
-                  )),
+                        color: Colors.white,
+                        fontSize: 16,
+                        decoration: TextDecoration.underline),
+                  ),
+                )
+              ],
             ),
-            FlatButton(
-              onPressed: () async {
-                Navigator.pushReplacementNamed(context, '/loading');
-              },
-              child: Text(
-                'Skip for now',
-                maxLines: 3,
-                softWrap: true,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    decoration: TextDecoration.underline),
-              ),
-            )
-          ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
